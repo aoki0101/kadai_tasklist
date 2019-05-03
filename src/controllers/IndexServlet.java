058b66b499679890c8ff7c3dc580bf6d8657a841
ryuta.aoki <tasevgam174@gmail.com>
2019-05-03 10:46:23 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,18 +35,36 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	    EntityManager em = DBUtil.createEntityManager();
-
+	    
+	    int page = 1;
+	    try{
+	        page = Integer.parseInt(request.getParameter("page"));
+	    }catch(NumberFormatException e) {}
+	    
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+                                   .setFirstResult(15 * (page-1))
+                                   .setMaxResults(15)
                                    .getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
-
+//        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+        long tasks_count = (long)em.createNamedQuery("getTasksCount",Long.class)
+                                      .getSingleResult();
+        
         em.close();
         
         request.setAttribute("tasks",tasks );
-        
+        request.setAttribute("tasks_count",tasks_count );
+        request.setAttribute("page",page );
+        //フラッシュメッセージがセッションスコープにセットされていたら
+        //リクエストスコープに保存する
+        if(request.getSession().getAttribute("flush") != null){
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
         RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
 	}
+
+    
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
